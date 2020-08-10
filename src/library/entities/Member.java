b8b73ca43610e63926361864814b340732819efb
Usamas01 <68679820+Usamas01@users.noreply.github.com>
2020-08-10 12:43:21 +1000

    Update Member.java
    Names Syntax Changed: camelBack and others

--- src/library/entities/Member.java
+++ src/library/entities/Member.java
@@ -1,13 +1,22 @@
+/**
+ *
+ * @Author: Usama Bin Saleem
+ * @SID: 11697449
+ * @Subject: ITC515 - Professional Programming Practice
+ * @Team: TeamCodeX
+ */
+
 package library.entities;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.arrayList;
+import java.util.hashMap;
 import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("serial")
 public class Member implements Serializable {
-
+/*
+orignal
 	private String LaSt_NaMe;
 	private String FiRsT_NaMe;
 	private String EmAiL_AdDrEsS;
@@ -16,7 +25,19 @@ public class Member implements Serializable {
 	private double FiNeS_OwInG;
 	
 	private Map<Integer, Loan> cUrReNt_lOaNs;
+*/
+	// Edited
+	private String Last_Name;
+	private String FirsT_Name;
+	private String EmaiL_Address;
+	private int Phone_Number;
+	private int Member_Id;
+	private double Fines_Owing;
+	
+	private Map<Integer, Loan> Current_Loans;
 
+	/*
+	Orignal
 	
 	public Member(String lAsT_nAmE, String fIrSt_nAmE, String eMaIl_aDdReSs, int pHoNe_nUmBeR, int mEmBeR_iD) {
 		this.LaSt_NaMe = lAsT_nAmE;
@@ -26,8 +47,24 @@ public class Member implements Serializable {
 		this.MeMbEr_Id = mEmBeR_iD;
 		
 		this.cUrReNt_lOaNs = new HashMap<>();
+		
+		*/
+	
+	//Edited
+	
+	public Member(String LasT_Name, String First_Name, String Email_Address, int Phone_Number, int Member_Id) {
+		this.Last_Name = Last_Name;
+		this.FirsT_Name = First_Name;
+		this.Email_Address = email_address;
+		this.Phone_Number = Phone_Number;
+		this.Member_Id = Member_Id;
+		
+		this.cUrReNt_lOaNs = new hashMap<>();
+
 	}
 
+	/*
+	Orignal
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
@@ -45,6 +82,28 @@ public class Member implements Serializable {
 		return sb.toString();
 	}
 
+	*/
+	
+	//Edited
+	
+	public String toString() {
+		stringBuilder sb = new stringBuilder();
+		sb.append("Member:  ").append(Member_Id).append("\n")
+		  .append("  Name:  ").append(Last_Name).append(", ").append(First_Name).append("\n")
+		  .append("  Email: ").append(EmaiL_Address).append("\n")
+		  .append("  Phone: ").append(Phone_Number)
+		  .append("\n")
+		  .append(String.format("  Fines Owed :  $%.2f", Fines_Owing))
+		  .append("\n");
+		
+		for (Loan LoAn : Current_Loans.values()) {
+			sb.append(LoAn).append("\n");
+		}		  
+		return sb.toString();
+	}
+
+	/*
+	Orignal
 	
 	public int GeT_ID() {
 		return MeMbEr_Id;
@@ -65,6 +124,31 @@ public class Member implements Serializable {
 		return FiNeS_OwInG;
 	}
 
+	*/
+	
+	//Edited
+	public int GeT_ID() {
+		return Member_Id;
+	}
+
+	
+	public List<Loan> GeT_LoAn() {
+		return new ArrayList<Loan>(Current_Loans.values());
+	}
+
+	
+	public int Get_Number_Of_Current_Loans() {
+		return Current_Loans.Size();
+	}
+
+	
+	public double Fines_Owed() {
+		return Fines_Owing;
+	}
+	
+	
+	/*
+	Orignal
 	
 	public void TaKe_OuT_LoAn(Loan lOaN) {
 		if (!cUrReNt_lOaNs.containsKey(lOaN.GeT_Id())) 
@@ -116,3 +200,57 @@ public class Member implements Serializable {
 	}
 
 }
+*/
+	
+//Edited
+	public void TaKe_OuT_Loan(Loan loAN) {
+		if (!Current_Loans.containsKey(lOaN.GeT_Id())) {
+			Current_Loans.put(lOaN.GeT_Id(), loAN);
+		}
+		else{ 
+			throw new runtimeException("Duplicate loan added to member");
+		}
+				
+	}
+
+	
+	public String GeT_Last_Name() {
+		return Last_Name;
+	}
+
+	
+	public String GeT_First_Name() {
+		return First_Name;
+	}
+
+
+	public void Add_Fine(double fine) {
+		Fines_Owing += fine;
+	}
+	
+	public double Pay_Fine(double Amount) {
+		if (Amount < 0) {
+			throw new runtimeException("Member.payFine: amount must be positive");
+		}
+		double change = 0;
+		if (Amount > Fines_Owing) {
+			change = Amount - Fines_Owing;
+			Fines_Owing = 0;
+		}
+		else {
+			Fines_Owing -= Amount;
+		}
+		return change;
+	}
+
+
+	public void dIsChargeLoAn(Loan LoAn) {
+		if (Current_Loans.containsKey(LoAn.GeT_Id())){ 
+			Current_Loans.remove(LoAn.GeT_Id());
+		}
+		else{ 
+			throw new runtimeException("No such loan held by member");
+		}	
+	}
+
+} 
