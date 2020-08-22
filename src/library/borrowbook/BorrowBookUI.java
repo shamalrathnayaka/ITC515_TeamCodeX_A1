4464b7b24d53e3cd2d6ef1ef65af7a6de00ac994
Shamal Rathnayaka <dfnmahesh@gmail.com>
2020-08-22 17:31:32 +1000

    Change the indentation according to the style guidelines

--- src/library/borrowbook/BorrowBookUI.java
+++ src/library/borrowbook/BorrowBookUI.java
@@ -6,8 +6,8 @@
  * @Team: TeamCodeX
  */
 package library.borrowbook;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class BorrowBookUI {
 	
@@ -17,30 +17,25 @@ public class BorrowBookUI {
 	private Scanner input;
 	private UiState state;
 
-	
 	public BorrowBookUI(BorrowBookControl control) {
 		this.control = control;
 		input = new Scanner(System.in);
 		state = UiState.INITIALISED;
 		control.setUi(this);
 	}
-
 	
 	private String input(String prompt) {
 		System.out.print(prompt);
 		return input.nextLine();
-	}	
-		
+	}
 		
 	private void output(Object object) {
 		System.out.println(object);
 	}
-	
-			
+
 	public void setState(UiState state) {
 		this.state = state;
 	}
-
 	
 	public void run() {
 		output("Borrow Book Use Case UI\n");
@@ -53,7 +48,6 @@ public class BorrowBookUI {
 				output("Borrowing Cancelled");
 				return;
 
-				
 			case READY:
 				String memStr = input("Swipe member card (press <enter> to cancel): ");
 				if (memStr.length() == 0) {
@@ -69,13 +63,11 @@ public class BorrowBookUI {
 				}
 				break;
 
-				
 			case RESTRICTED:
 				input("Press <any key> to cancel");
 				control.cancel();
 				break;
-			
-				
+
 			case SCANNING:
 				String bookStringInput = input("Scan Book (<enter> completes): ");
 				if (bookStringInput.length() == 0) {
@@ -90,24 +82,20 @@ public class BorrowBookUI {
 					output("Invalid Book Id");
 				} 
 				break;
-					
-				
+
 			case FINALISING:
 				String ans = input("Commit loans? (Y/N): ");
 				if (ans.toUpperCase().equals("N")) {
 					control.cancel();
-					
 				} else {
 					control.commitLoans();
 					input("Press <any key> to complete ");
 				}
 				break;
-				
-				
+
 			case COMPLETED:
 				output("Borrowing Completed");
 				return;
-	
 				
 			default:
 				output("Unhandled state");
@@ -116,10 +104,8 @@ public class BorrowBookUI {
 		}		
 	}
 
-
 	public void display(Object object) {
 		output(object);
 	}
-
 
 }
